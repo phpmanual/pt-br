c98351b7854634c25d27300b02a6847652ccf9bb
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-21 13:51:56 -0200

    Update .compile.sh

--- .compile.sh
+++ .compile.sh
@@ -4,7 +4,7 @@ set -e
 # make the script display the commands it runs to help debugging failures
 set -x
 
-svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR &>/dev/null
+svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR ./doc-pt_BR &>/dev/null
 
 tree -L 2
 
