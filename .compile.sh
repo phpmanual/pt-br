c963b2ea39d75b878fb90e612f4b2c552d30f1b2
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-21 11:10:44 -0200

    Decrease verbosity of commands for travis log

--- .compile.sh
+++ .compile.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR
+echo ""
+echo "svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR &>/dev/null"
+svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR &>/dev/null
+echo ""
+echo "Done."
 
+echo ""
+echo "./generate-html.php > out/index.html"
 ./generate-html.php > out/index.html
-
+echo ""
+echo "Done."
