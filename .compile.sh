c23ac605a1169d21703d50a9830eb86e7ded6638
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-21 18:00:52 -0200

    Change .compile.sh to not redirect svn checkout std error

--- .compile.sh
+++ .compile.sh
@@ -4,10 +4,7 @@ set -e
 # make the script display the commands it runs to help debugging failures
 set -x
 
-svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR doc-pt_BR &>/dev/null \
-    \
+svn checkout https://svn.php.net/repository/phpdoc/modules/doc-pt_BR doc-pt_BR 1> /dev/null \
     && tree -L 2 \
-    \
     && mkdir -p out \
-    \
     && ./generate-html.php > out/index.html
